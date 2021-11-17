9a065963a2bc28f24132d5861f76416b5aad019f
kanchanCode <jkanchan2911@gmail.com>
2021-11-17 07:18:36 +0530

    added code

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QueueAssignment
+{
+    class Program
+    {
+        public class Node
+        {
+           
+            public int data;
+           
+            public Node next;
+
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+            static void Main(string[] args)
+        {
+            queueFunctions obj = new queueFunctions();
+            obj.Enqueue(56);
+            obj.Enqueue(30);
+            obj.Enqueue(70);
+            obj.Display();
+            obj.Dequeue();
+            Console.WriteLine("Queue after dequeue :");
+            obj.Display();
+        }
+    }
+}
---
+++ queueFunctions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static QueueAssignment.Program;
+
+namespace QueueAssignment
+{
+    class queueFunctions
+    {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+  
+            }
+            Console.WriteLine(node.data + " is inserted into Queue");
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                
+                Console.Write(temp.data+" ");
+                temp = temp.next;
+            }
+       
+        }
+
+        internal void Dequeue()
+        {
+            Node temp = this.head;
+            if (temp != null)
+            {
+                this.head = head.next;
+                temp = null;
+            }
+            else
+            {
+                Console.WriteLine("List is empty");
+            
+            }
+            
+        }
+        
+    }
+}
